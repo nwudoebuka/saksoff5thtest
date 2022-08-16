67b9015084cc38473df77303e5a20d8e2769ceca
Nwudo Chukwuebuka Anthony <chukwuebuka.nwudo@deimos.co.za>
2022-08-16 22:50:19 +0100

    first commit

---
+++ SaksOff5thTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaksOff5thTest
+//
+//  Created by Anthony Ebuka Nwudo on 16/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ SaksOff5thTestUITests/SaksOff5thTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SaksOff5thTestUITestsLaunchTests.swift
+//  SaksOff5thTestUITests
+//
+//  Created by Anthony Ebuka Nwudo on 16/08/2022.
+//
+
+import XCTest
+
+class SaksOff5thTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
